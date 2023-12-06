7300015e907a16876d97ec2732a3dc3ead627b1d
CassandraK <ko.cassie@gmail.com>
2023-12-06 16:53:48 -0500

    feat: added cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 const { createPaletteFromColor } = require('palettey');
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
